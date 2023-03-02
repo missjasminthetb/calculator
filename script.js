4e57ab3ce2ca5d4022eb904125644c933fc3253e
Missjasminthetb <salamijasmin@gmail.com>
2023-03-02 14:44:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+let result = document.querySelector(".result")
+let numberButtons = document.querySelectorAll('.numbers');
+let form = document.querySelector('form');
+let minus = document.querySelector("#minus");
+let add = document.querySelector(".addition");
+let del = document.querySelector(".delete");
+let multiply = document.querySelector(".multiply");
+let submit = document.querySelector(".submit");
+let reset = document.querySelector(".reset")
+
+
+let sum = 0;
+
+values.forEach(  value => value.addEventListener('click', valueFunction))
+
+function minusFunction (){
+
+}
+
+form.addEventListener('submit',()=>{
+
+})
+
+
+
+
+
+
+
+
